cf702211704bb8d3eaa5c55694a80469d0282ea9
Muskaan Fatima <muskaanfatima118@gmail.com>
2025-11-30 18:35:11 -0500

    Update name in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # REPLACE [YourLastname] with your actual name
-    return "Hello from [YourLastname] ECS Container."
+    return "Hello from fatima ECS Container."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
